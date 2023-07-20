35a542a3c9d2d772dd800159ab1c2de106fd9ca4
Felipe Dalbert <felipeoficial195@gmail.com>
2023-07-19 23:20:36 -0300

    create basic files structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TodoList from "./Todo-list";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+       
+    </React.StrictMode>
+)
