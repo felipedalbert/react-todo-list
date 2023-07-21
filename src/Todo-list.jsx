bbf45a13b7c26ebfd3a6ba550bce1dbc5f7117cc
Felipe Dalbert <felipeoficial195@gmail.com>
2023-07-21 00:39:31 -0300

    feat(TodoList): logica da adição do todo

--- src/Todo-list.jsx
+++ src/Todo-list.jsx
@@ -1,25 +1,52 @@
+import { useState, useEffet } from 'react';
 import './Todo-list.css'
+import todoIllustration from './assets/todoIllustration.svg';
 
 function TodoList(){
+
+    const[lista, setLista] = useState([])
+    const [novoItem, setNovoItem] = useState("")
+
+    function addItem(form){
+        form.preventDefault()
+
+        if(!novoItem) return
+
+        setLista((prevState) => [...prevState, {text: novoItem, isCompleted: false}])
+        setNovoItem("")
+        document.getElementById('input-entrada').focus()
+    }
+
     return (
         <div>
             <h1>Lista de tarefas</h1>
-            <form>
-                <input type="text" placeholder='Adicione uma tarefa'/>
+
+            <form onSubmit={addItem}>
+                <input
+                    id="input-entrada" 
+                    type="text"
+                    value={novoItem} 
+                    onChange={(e) => {setNovoItem(e.target.value)}}
+                    placeholder='Adicione uma tarefa'
+                />
                 <button type='submit' className='add'>Add</button>
             </form>
-            <div className='lista-tarefas'>
-                <div className='item'>
-                    <span>Tarefa de exemplo</span>
-                    <button className='del'>Deletar</button>
-                </div>
-                <div className='item check'>
-                    <span>Tarefa de exemplo</span>
-                    <button className='del'>Deletar</button>
-                </div>
+
+            <div className='lista-tarefas' style={{textAlign: 'center'}}>
+                
+                    {
+                        lista.length < 1 ? <img src={todoIllustration} style={{marginTop: "-65px"}} />
+                        : lista.map((item, index) =>(
+                            <div className='item' key={index}>
+                                <span>{item.text}</span>
+                                <button className='del'>Deletar</button>
+                            </div>   
+                        ))
+                    }
                 
             </div>
-            <button className='deleteAll'>Deletar Tudo</button>
+
+            {lista.length > 0 && <button className='deleteAll' >Deletar Tudo</button>}
         </div>
         
     )
